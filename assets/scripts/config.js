894a76edded61e5c2d6ce95246a4170b6c60b856
gandreottola <gandreottola09@gmail.com>
2019-08-19 20:46:44 -0400

    Changed production url
    - Changed to pic-container.herkouapp

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://the-coherent-chickens-heroku.herokuapp.com',
+  production: 'https://pic-container.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
